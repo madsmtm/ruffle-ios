e6754096fdc6d10c9ec6ea29b4d5c1b7a7253860
Mads Marquart <mads@marquart.dk>
2024-09-13 00:22:57 +0200

    Set up logging

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 A normal person might have wrapped the Rust in some `extern "C" fn`s, and then used SwiftUI, or at least Objective-C for the UI shell. I would probably recommend that for most use-cases.
 
 I'm developing [`objc2`](https://github.com/madsmtm/objc2) though, and I want to improve the user-interface of that, so I decided to be a bit unortodox, and do everything in Rust.
+
+## Testing
+
+Run on Mac Catalyst with:
+```
+cargo +nightly bundle --target=aarch64-apple-ios-macabi && ./target/aarch64-apple-ios-macabi/debug/bundle/ios/Ruffle.app/ruffle-ios
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
+    // Emit logging to either OSLog or stderr, depending on if using Mac
+    // Catalyst or native.
+    // TODO: If running Mac Catalyst under Xcode
+    let filter = log::LevelFilter::Debug;
+    if cfg!(target_abi = "macabi") {
+        simple_logger::SimpleLogger::new()
+            .with_level(filter)
+            .env()
+            .init()
+            .unwrap();
+    } else {
+        oslog::OsLogger::new(module_path!())
+            .level_filter(filter)
+            .init()
+            .unwrap();
+    }
+
     println!("Hello, world!");
 }
